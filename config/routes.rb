affe598520cac31835540222f55ef9e195944ab8
Prima Aulia Gusta <primaulia@gmail.com>
2021-11-22 10:08:34 +0800

    setup our first path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  # VERB + PATH + CONTROLLER + ACTION
+  # VERB </PATH>, to: CONTROLLER#ACTION
+
+  get 'about', to: "pages#about"
 end
